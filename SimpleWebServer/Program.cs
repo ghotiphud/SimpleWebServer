33031ae18b52d1028afb14595ba2590e0a908c32
Yale Cason <yale.cason@gmail.com>
2016-05-25 21:26:19 -0400

    Hello World!

---
+++ SimpleWebServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8888";
+            using (WebApp.Start(url, Configuration))
+            {
+                Console.WriteLine("Hosting on {0}", url);
+                Console.WriteLine("Press [enter] to Quit");
+                Console.ReadLine();
+            }
+        }
+
+        
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public static void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Run(context => {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
